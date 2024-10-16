6f7a9ff6765c241596830166d23867988beec530
nguyenquocnhu <nquocnhu95it@gmail.com>
2024-10-16 08:11:26 +0700

    finished crudAPI

--- src/services/userApiService.js
+++ src/services/userApiService.js
@@ -109,17 +109,45 @@ const createNewUser = async (data) => {
 };
 const updateUser = async (data) => {
   try {
+    //validate group
+    console.log('Check data:', data)
+    if(!data.groupId){
+      return {
+        EM: "GroupId is empty or something wrong",
+        EC: 1,
+        DT:"Group"
+      }
+    }
     let user = await db.User.findOne({
-      where: { id: data.id },
-    });
-    if (user) {
-      //update
-      user.save({});
+      where: {id: data.id}
+    })
+    if(user) {
+      await user.update({
+        //hack
+        username: data.username,
+        address: data.address,
+        sex: data.sex,
+        groupId: data.groupId
+      })
+      return {
+        EM: "User was updated successfully",
+        EC: 0,
+        DT: "",
+      }
     } else {
-      //not found
+      return {
+        EM: "User not found",
+        EC: 0,
+        DT:""
+      }
     }
   } catch (e) {
     console.log(e);
+    return {
+      EM: "something wrong with services",
+      EC: 1,
+      DT: [],
+    };
   }
 };
 // DELETE MODAL HANDLING 
